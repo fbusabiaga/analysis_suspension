27af6fa6b9899f603484d6e2931b0a5a3a4f8c46
Florencio Balboa Usabiaga <fbusabiaga@gmail.com>
2022-06-12 18:18:52 +0200

    more efficient functions, pythonic way

--- simulation_analysis_offline_tilt_angle.py
+++ simulation_analysis_offline_tilt_angle.py
@@ -18,6 +18,8 @@ if __name__ == '__main__':
                   '/home/fbalboa/simulations/RigidMultiblobsWall/chiral/data/run3000/run3007/run3007.5.3.1.superellipsoid_run3007.5.3.1.config']
   structure = 'superellipsoid'
   num_frames = 10000
+  tilt_vtk = False
+  grid = np.array([-100, 100, 10, -100, 100, 10, 0, 0, 1])
 
   # Read inputfile
   name_input = file_prefix + '.inputfile' 
@@ -45,6 +47,8 @@ if __name__ == '__main__':
 
   # Loop over config files
   x = sa.read_config_list(files_config, print_name=True)
+  if num_frames < x.shape[0]:
+    x = x[0:num_frames]
 
   # Concatenate config files
   t = np.arange(x.shape[0]) * dt_sample
@@ -61,29 +65,73 @@ if __name__ == '__main__':
   f_handle.write('# Angular velocity direction in rotating frame vector given by the vectors z, B and z \times B. \n')
   f_handle.write('# Columns: time, omega_z, omega_B, omega_{z \times B}, omega_magnitude. \n')
 
-  # Compute tilt angle
+  # Compute tilt angle versus time
   z = np.array([0, 0, 1.0])
   for i, vi in enumerate(velocity):
     B0_hat = np.array([np.cos(omega * t[i]), np.sin(omega * t[i]), 0])
     B = np.dot(R_B, B0_hat)
-    omega_z = 0
-    omega_B = 0
-    omega_perp = 0
-    omega_norm_avg = 0
-    for j, vij in enumerate(vi):
-      # Get angular velocity direction
-      omega_norm = np.linalg.norm(vij[3:])
-      omega_vec_hat = vij[3:] / omega_norm
 
-      # Save components of angular velocity in rotating frame
-      omega_z += np.dot(omega_vec_hat, z)
-      omega_B += np.dot(omega_vec_hat, B)
-      omega_perp += np.dot(omega_vec_hat, np.cross(z, B))
-      omega_norm_avg += omega_norm
+    # Get angular velocity direction and magnitude
+    omega_norm = np.linalg.norm(vi[:,3:], axis=1)
+    omega_vec_hat = vi[:,3:] / omega_norm[:,None]      
+    omega_z = np.einsum('ij,j->', omega_vec_hat, z) / vi.shape[0]
+    omega_B = np.einsum('ij,j->', omega_vec_hat, B) / vi.shape[0]
+    omega_perp = np.einsum('ij,j->', omega_vec_hat, np.cross(z,B)) / vi.shape[0]
+    omega_norm_avg = np.einsum('i->', omega_norm) / vi.shape[0]
 
     # Save time value
-    omega_z /= vi.shape[0]
-    omega_B /= vi.shape[0]
-    omega_perp /= vi.shape[0]
-    omega_norm_avg /= vi.shape[0]
     f_handle.write(str(i * dt_sample) + ' ' +  str(omega_z) + ' ' +  str(omega_B) + ' ' +  str(omega_perp) + ' ' +  str(omega_norm_avg) + '\n')
+
+  # Save tilt to vtk file
+  if tilt_vtk:
+    grid = np.reshape(grid, (3,3)).T
+    grid_length = grid[1] - grid[0]
+    grid_points = np.array(grid[2], dtype=np.int32)
+    num_points = grid_points[0] * grid_points[1] * grid_points[2]
+
+    # Set grid coordinates
+    dx_grid = grid_length / grid_points
+    grid_x = np.array([grid[0,0] + dx_grid[0] * (x+0.5) for x in range(grid_points[0])])
+    grid_y = np.array([grid[0,1] + dx_grid[1] * (x+0.5) for x in range(grid_points[1])])
+    grid_z = np.array([grid[0,2] + dx_grid[2] * (x+0.5) for x in range(grid_points[2])])
+    # Be aware, x is the fast axis.
+    zz, yy, xx = np.meshgrid(grid_z, grid_y, grid_x, indexing = 'ij')
+    grid_coor = np.zeros((num_points, 3))
+    grid_coor[:,0] = np.reshape(xx, xx.size)
+    grid_coor[:,1] = np.reshape(yy, yy.size)
+    grid_coor[:,2] = np.reshape(zz, zz.size)
+ 
+    for i, vi in enumerate(velocity):
+      # Create vector for omega orientation
+      grid_omega_z = np.zeros_like(grid_coor)
+      grid_omega_B = np.zeros_like(grid_coor)
+      grid_omega_perp = np.zeros_like(grid_coor)
+      grid_omega_norm = np.zeros_like(grid_coor) 
+      B0_hat = np.array([np.cos(omega * t[i]), np.sin(omega * t[i]), 0])
+      B = np.dot(R_B, B0_hat)
+
+      # Compute center of mass
+      xi = x[i]
+      r_cm = np.mean(xi[:,0:3], axis=0)
+      print('r_cm = ', r_cm)
+      
+      for j, vij in enumerate(vi):
+        # Get angular velocity direction
+        omega_norm = np.linalg.norm(vij[3:])
+        omega_vec_hat = vij[3:] / omega_norm
+        
+        # Save components of angular velocity in rotating frame
+        omega_z = np.dot(omega_vec_hat, z)
+        omega_B = np.dot(omega_vec_hat, B)
+        omega_perp = np.dot(omega_vec_hat, np.cross(z, B))
+        omega_norm_avg = omega_norm
+
+        # # Get cell index
+        # xij = x[i,j]
+        # ix = xij[0] / 
+
+      # Save time value
+      omega_z /= vi.shape[0]
+      omega_B /= vi.shape[0]
+      omega_perp /= vi.shape[0]
+      omega_norm_avg /= vi.shape[0]
