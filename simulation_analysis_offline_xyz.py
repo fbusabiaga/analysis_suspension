921bc83dd83ce72939b0af609f297f4c585f287d
Florencio Balboa Usabiaga <fbusabiaga@gmail.com>
2022-06-12 16:13:27 +0200

    new quaternion file

--- simulation_analysis_offline_xyz.py
+++ simulation_analysis_offline_xyz.py
@@ -4,20 +4,20 @@ Script to save xyz files.
 import numpy as np
 import sys
 import simulation_analysis as sa
-
+from quaternion import Quaternion
 
 if __name__ == '__main__':
   # Set parameters
-  file_prefix = '/home/fbalboa/simulations/RigidMultiblobsWall/chiral/data/run2000/run2211/run2211.6.9.0'
-  files_config = ['/home/fbalboa/simulations/RigidMultiblobsWall/chiral/data/run2000/run2211/run2211.6.9.0.rod_run2211.6.9.0.config',
-                  '/home/fbalboa/simulations/RigidMultiblobsWall/chiral/data/run2000/run2211/run2211.6.9.1.rod_run2211.6.9.1.config']
-  structure = 'rod'
-  name_vertex = '/home/fbalboa/sfw/RigidMultiblobsWall/multi_bodies/examples/chiral/Structures/rod_Lg_1.664_Rg_0.416_Nx_6_Ntheta_6.vertex'
+  file_prefix = '/home/fbalboa/simulations/RigidMultiblobsWall/chiral/data/run3000/run3007/run3007.5.3.0'
+  files_config = ['/home/fbalboa/simulations/RigidMultiblobsWall/chiral/data/run3000/run3007/run3007.5.3.0.superellipsoid_run3007.5.3.0.config',
+                  '/home/fbalboa/simulations/RigidMultiblobsWall/chiral/data/run3000/run3007/run3007.5.3.1.superellipsoid_run3007.5.3.1.config']
+  structure = 'superellipsoid'
+  name_vertex = '/home/fbalboa/sfw/RigidMultiblobsWall/multi_bodies/examples/chiral/Structures/superellipsoid_Lg_1.368_r_3.9_N_26.vertex'
   num_frames = 1000
   save_blobs = True
   save_dipole = True
   save_velocity = False
-  save_dat_index = [0,4]
+  save_dat_index = []
 
   # Read inputfile
   name_input = file_prefix + '.inputfile' 
@@ -34,6 +34,10 @@ if __name__ == '__main__':
   n_save = int(read.get('n_save'))
   dt_sample = dt * n_save
   eta = float(read.get('eta'))
+  if 'quaternion_B' in read:
+    print('quaternion_B = ', read.get('quaternion_B'))
+    quaternion_B = Quaternion(np.fromstring(read.get('quaternion_B'), sep=' ') / np.linalg.norm(np.fromstring(read.get('quaternion_B'), sep=' ')))
+    R_B = quaternion_B.rotation_matrix()
   print('file_prefix = ', file_prefix)
   print('dt          = ', dt)
   print('dt_sample   = ', dt_sample)
@@ -75,12 +79,12 @@ if __name__ == '__main__':
     omega = float(read.get('omega'))
     dipole = np.zeros((x.shape[1], 3))
     dipole[:] = dipole_0
-    B = np.zeros((x.shape[0], 3))
-    B[:,0] = B0 * np.cos(omega * t)
-    B[:,2] = -B0 * np.sin(omega * t)
+    B_xy = np.zeros((x.shape[0], 3))
+    B_xy[:,0] = B0 * np.cos(omega * t)
+    B_xy[:,1] = B0 * np.sin(omega * t)
+    B = np.einsum('ij,kj->ki', R_B, B_xy)  
     name = file_prefix + '.' + structure + '.dipole.xyz'    
     print('B0    = ', B0)
     print('omega = ', omega)
     print('name  = ', name)
-    # print('B     = ', B)
     sa.save_xyz(x, np.zeros(3), name, num_frames=num_frames, letter=structure[0].upper(), body_frame_vector=dipole, global_vector=B, header='Columns: r, dipole, magnetic field')
